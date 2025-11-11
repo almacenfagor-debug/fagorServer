8f49ffac0378a9d8d9a634f1f9c1bec8d95d3027
Your Name <you@example.com>
2025-11-11 16:07:41 -0500

    db configuration

--- utils/db.js
+++ utils/db.js
@@ -2,12 +2,12 @@ const { Sequelize } = require("sequelize");
 require('dotenv').config();
 
 const db = new Sequelize(process.env.EX_DATABASE_URL,{
-  database: "fagorwarehousedb",
+  database: "fagordb",
   port: 5432,
   host: "localhost",
   dialect: "postgres",
-  username: "postgres",
-  password: "root",
+  username: "fagordb_user",
+  password: "2cjpUepETKUcZNJ82fuXumj8oARcsFS4",
   logging: false,
 });
 
