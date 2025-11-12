292aa49a93609c3fbfebb690981b4655d9ad96bc
Your Name <you@example.com>
2025-11-12 07:41:00 -0500

    db configuration

--- utils/db.js
+++ utils/db.js
@@ -2,13 +2,16 @@ const { Sequelize } = require("sequelize");
 require('dotenv').config();
 
 const db = new Sequelize(process.env.EX_DATABASE_URL,{
-  database: "fagordb",
-  port: 5432,
-  host: "localhost",
+  database: "railway",
+  port: 17410,
+  host: "interchange.proxy.rlwy.net",
   dialect: "postgres",
-  username: "fagordb_user",
-  password: "2cjpUepETKUcZNJ82fuXumj8oARcsFS4",
+  username: "postgres",
+  password: "tqYdfvZhYWhVVojBrjwJBgxaaKyDYWxX",
   logging: false,
+
+  usuario: postgres
+
 });
 
 module.exports = db;
