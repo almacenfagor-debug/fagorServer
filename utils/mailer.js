4dd3b4ec179f414e6cdbca2052a008012c66e581
Your Name <you@example.com>
2025-11-12 15:43:00 -0500

    testing service 3

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,16 +1,16 @@
 
-
-const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
-  port: 465,
   host: "smtp.gmail.com",
+  port: 465,
   secure: true,
   auth: {
     user: "almacenfagor@gmail.com",
     pass: "lbnfidbsznhlbsqu",
   },
+  logger: true,
+  debug: true, // 
 });
+
 
 module.exports = transporter;
 
