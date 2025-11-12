30d480024438a3dfb37c791247c435d25b0668f6
Your Name <you@example.com>
2025-11-12 14:33:31 -0500

    password aplication

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,12 +3,12 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  port: 587,
+  port: 465,
   host: "smtp.gmail.com",
   secure: true,
   auth: {
     user: "almacenfagor@gmail.com",
-    pass: "ngdzbnkmyeonrvsw",
+    pass: "lbnfidbsznhlbsqu",
   },
 });
 
